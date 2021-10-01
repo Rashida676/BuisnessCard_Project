a64778dfddd9b359ee1f23d98c319f3accc9f9d6
Rashida Haris <69212081+Rashida676@users.noreply.github.com>
2021-10-01 16:30:18 +0400

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,12 @@
+<?php
+//start the session
+//session_start();
+//Create constants to save database credentials
+define('LOCALHOST','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD',"");
+define('DB_NAME','buisnesscard');
+define('SITEURL', 'http://localhost:8080/buisnesscard/');
+
+
+?>
